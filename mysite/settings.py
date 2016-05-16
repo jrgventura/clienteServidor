b686f4dab825a7cd503341e671ac23804037752d
jorge.venturag@outlook.com <jorge.venturag@outlook.com>
2016-05-16 02:17:02 -0500

    Consulta a la bd sql en local

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'Planilla',
+        'ENGINE': 'sqlserver_ado',
+        'HOST': 'dbserver\\ss2012',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
