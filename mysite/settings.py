ccd3c8409245d7b25651aae89693dc5e26c80370
jorge.venturag@outlook.com <jorge.venturag@outlook.com>
2016-05-19 00:44:27 -0500

    crud django

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-     }
+    # 'default': {
+    #      'ENGINE': 'django.db.backends.sqlite3',
+    #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #  }
     # 'default': {
     #     'NAME': 'Planilla',
     #     'ENGINE': 'sqlserver_ado',
@@ -88,6 +88,13 @@ DATABASES = {
     #     'USER': '',
     #     'PASSWORD': '',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '',
+    }
 }
 
 
